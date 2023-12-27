ce5d18ff6ae1918553a335c0ce45ac2d3048cbb7
B1naryo <sandro520099@hotmail.com>
2023-12-27 13:02:24 -0300

    Update config.js

--- bulk/js/config.js
+++ bulk/js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x5D606CE32b5c4A06609AF2f840B7FA1d1c1dfBa5";
 
 
 var BinanceFee=0.01;
